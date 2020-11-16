7b37e3036b76fe0c49011df6760b765aed3ae930
yifengjin89 <55426354+yifengjin89@users.noreply.github.com>
2020-11-16 03:23:45 -0500

    added square test

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,10 @@
 const sum = require('./sum');
+const square = require('./square')
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('square 2 to equal 4', () => {
+  expect(square(2)).toBe(4);
 });
